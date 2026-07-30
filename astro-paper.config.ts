93be66429d5135ec2c6c583a64ebf43b51362ff5
Darko Dekan <darko.dekan@protonmail.com>
2026-07-30 20:01:12 +0200

    Working astropaper installation

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,13 +3,13 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    title: "Darko Dekan",
+    description: "My corner of the internet dedicated to pure mathematics, programming, and tutoring.",
+    author: "Darko Dekan",
+    profile: "https://darkodekan.com/about/",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Europe/Belgrade",
     dir: "ltr",
   },
   posts: {
@@ -29,13 +29,13 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/darkodekan" },
+    { name: "x",        url: "https://x.com/darkodean" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/darkodekan/" },
+    { name: "mail",     url: "mailto:darkodekan@gmail.com" },
   ],
   shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "whatsapp", url: "https://wa.me/?text=381621005941" },
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
     { name: "x",        url: "https://x.com/intent/post?url=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
